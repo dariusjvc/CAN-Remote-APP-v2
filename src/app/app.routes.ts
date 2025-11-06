69207ddeefcb6c9fe7dcb20d9bab285c9203049c
dariusjvc <dario.val.cast@gmail.com>
2025-11-06 19:00:14 +0100

    Added first version of the main/dashboard and main/device

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,16 @@ export const routes: Routes = [
     path: 'device',
     loadComponent: () => import('./device/device.page').then( m => m.DevicePage)
   },
+  {
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then( m => m.MainPage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./main/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'device',
+    loadComponent: () => import('./main/device/device.page').then( m => m.DevicePage)
+  },
 ];
