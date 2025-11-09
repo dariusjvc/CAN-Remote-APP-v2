565d68f94656e6f6313704e445ebbe222149d36b
dariusjvc <dario.val.cast@gmail.com>
2025-11-09 19:40:51 +0100

    Added first version of the fingerprint

--- src/app/fingerprint/fingerprint.page.ts
+++ src/app/fingerprint/fingerprint.page.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+
+// Ionic Standalone imports (solo lo que usas en la plantilla)
+import {
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonItem,
+  IonText
+} from '@ionic/angular/standalone';
+
+import { AlertsService } from '../services/alerts.service';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-fingerprint',
   templateUrl: './fingerprint.page.html',
   styleUrls: ['./fingerprint.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonGrid, IonRow, IonItem, IonText],
 })
 export class FingerprintPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alerts: AlertsService,
+    private storage: StorageService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async ionViewWillEnter() {
+    await this.unLock();
   }
 
+  async unLock() {
+    const biometric = await this.storage.get<boolean>('biometric');
+
+    await this.alerts.fingerPrintAIO();
+  }
+
+  async removeData() {
+    await Promise.all([
+      this.storage.remove('user_pass'),
+      this.storage.remove('biometric'),
+    ]);
+    this.alerts.toastInfo('Data Removed!');
+  }
 }
