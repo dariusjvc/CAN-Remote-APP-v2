d3b4df0061b6ac3fb1233c51aec1559c0058e813
dariusjvc <dario.val.cast@gmail.com>
2025-12-06 19:02:04 +0100

    Add parameters in the plc-info component

--- src/app/main/dashboard/plc-info/plc-info.page.ts
+++ src/app/main/dashboard/plc-info/plc-info.page.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonCard, IonRow, IonGrid, IonMenuButton, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-plc-info',
   templateUrl: './plc-info.page.html',
   styleUrls: ['./plc-info.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonCard, IonRow, IonGrid, IonMenuButton, IonButtons],
 })
 export class PlcInfoPage implements OnInit {
+
+  public batteryVoltaje: string = "";
+  public batteryPercent: string = "";
+  public plcTemp: string = "";
+  public segWork: string = "";
+  public minWork: string = "";
+  public hoursWork: string = "";
+  public cyclesPartial: string = "";
+  public cyclesTotal: string = "";
+  public bypassNum: string = "";
+  public batteryPercent2: string = "";
+  // private updateSubscription: Subscription;
+
 
   constructor() { }
 
