e556c0de657c68273f261b66d880a5f009a75b6c
dariusjvc <dario.val.cast@gmail.com>
2025-12-06 18:37:20 +0100

    Add tabs change-device and dashboard and navegate to all of them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  { 
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then(m => m.LoginPage),
-  },
-    {
+  { path: 'login', loadComponent: () => import('./login/login.page').then(m => m.LoginPage) },
+  {
     path: 'device',
-    loadComponent: () => import('./device/device.page').then( m => m.DevicePage)
+    loadComponent: () => import('./device/device.page').then(m => m.DevicePage)
   },
-
   {
     path: 'main',
     loadComponent: () => import('./main/main.page').then(m => m.MainPage),
@@ -24,27 +19,7 @@ export const routes: Routes = [
       {
         path: 'change-device',
         loadComponent: () =>
-          import('./main/change-device/change-device.page').then(m => m.ChangeDevicePage),
-      },
-      {
-        path: 'plc-info',
-        loadComponent: () =>
-          import('./main/dashboard/plc-info/plc-info.page').then(m => m.PlcInfoPage),
-      },
-      {
-        path: 'extra-info',
-        loadComponent: () =>
-          import('./main/dashboard/extra-info/extra-info.page').then(m => m.ExtraInfoPage),
-      },
-      {
-        path: 'outputs',
-        loadComponent: () =>
-          import('./main/dashboard/outputs/outputs.page').then(m => m.OutputsPage),
-      },
-      {
-        path: 'inputs',
-        loadComponent: () =>
-          import('./main/dashboard/inputs/inputs.page').then(m => m.InputsPage),
+          import('./main/device/device.page').then(m => m.DevicePage),
       },
       {
         path: '',
@@ -53,20 +28,32 @@ export const routes: Routes = [
       },
     ],
   },
-
   {
     path: 'fingerprint',
-    loadComponent: () =>
-      import('./fingerprint/fingerprint.page').then(m => m.FingerprintPage),
+    loadComponent: () => import('./fingerprint/fingerprint.page').then(m => m.FingerprintPage)
   },
   {
     path: 'about',
-    loadComponent: () =>
-      import('./about/about.page').then(m => m.AboutPage),
+    loadComponent: () => import('./about/about.page').then(m => m.AboutPage)
   },
   {
     path: 'token',
-    loadComponent: () =>
-      import('./token/token.page').then(m => m.TokenPage),
+    loadComponent: () => import('./token/token.page').then(m => m.TokenPage)
+  },
+  {
+    path: 'plc-info',
+    loadComponent: () => import('./main/dashboard/plc-info/plc-info.page').then(m => m.PlcInfoPage)
+  },
+  {
+    path: 'extra-info',
+    loadComponent: () => import('./main/dashboard/extra-info/extra-info.page').then(m => m.ExtraInfoPage)
+  },
+  {
+    path: 'outputs',
+    loadComponent: () => import('./main/dashboard/outputs/outputs.page').then(m => m.OutputsPage)
+  },
+  {
+    path: 'inputs',
+    loadComponent: () => import('./main/dashboard/inputs/inputs.page').then(m => m.InputsPage)
   },
 ];
